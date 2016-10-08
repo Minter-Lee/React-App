d3aa681304afbf8d3d61d83dde6c90e527910b8c
lilei <lilei@beisen.com>
2016-10-08 19:18:17 +0800

    redux+es6 版本的todos

---
+++ app/scripts/redux-es6-index.js
@@ -0,0 +1,28 @@
+/*
+ * Title: 页面入口JS
+ * Author: MinterLee@hotmail.com
+ * CreateDate: 2016-10-08
+ * Description: 初始页面入口JS，用于页面主体结构向HTML的注入
+ */
+
+// React 基础使用
+import React from 'react';
+import { render } from 'react-dom';
+
+// react-redux 相关
+import { Provider } from 'react-redux';
+
+// 主容器视图
+import App from './containers/App';
+
+// 处理过的store构建器
+import configureStore from './stores/index';
+
+const store = configureStore();
+
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('container')
+	);
---
+++ app/scripts/redux-es6-todos/stores/index.js
@@ -0,0 +1,26 @@
+/*
+ * Title: 页面入口JS
+ * Author: MinterLee@hotmail.com
+ * CreateDate: 2016-10-08
+ * Description: store 构建器
+ */
+
+import reducers from './reducers/index';
+import { createStore } from 'redux';
+
+const configureStore = initialState => {
+	// 创建全局使用的store
+	const store = createStore(reducers, initialState);
+
+	//告知该文件及其所有关联被修正时，进行热替换，替换reducer
+	if (module.hot) {
+		module.hot.accpet('./reducers/index', () => {
+			// 重新加载reducer
+			const nextReducer = require('reducers/index');
+			// 替换reducer
+			store.replaceReducer(nextReducer);
+		})
+	}
+}
+
+export default configureStore;
